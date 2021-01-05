0e2844d9f11dde469bde4866437a347edd6e6003
ignatovasashka <ignatovasashka@gmail.com>
2021-01-05 14:48:29 +0200

    Add project files.

---
+++ RescueRegister/Controllers/MountaineerController.cs
@@ -0,0 +1,115 @@
+﻿using RescueRegister.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace RescueRegister.Controllers
+{
+    public class MountaineerController : Controller
+    {
+        private readonly RescueRegisterDbContext context;
+
+        public MountaineerController(RescueRegisterDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            using (var db = new RescueRegisterDbContext())
+            {
+                var allMountaineers = db.Mountaineers.ToList();
+                return View(allMountaineers);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Mountaineer mountaineer)
+        {
+            //if (string.IsNullOrEmpty(mountaineer))
+            //{
+            //    return RedirectToAction("Index");
+            //}
+
+            Mountaineer human = new Mountaineer
+            {
+                Name = mountaineer.Name,
+                Age = mountaineer.Age,
+                Gender = mountaineer.Gender,
+                LastSeenDate = mountaineer.LastSeenDate
+            };
+            using (var db = new RescueRegisterDbContext())
+            {
+                db.Mountaineers.Add(human);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var db = new RescueRegisterDbContext())
+            {
+                var humanToEdit = db.Mountaineers.FirstOrDefault(t => t.Id == id);
+                if (humanToEdit==null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return this.View(humanToEdit);
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Mountaineer mountaineer)
+        {
+            Mountaineer human = new Mountaineer
+            {
+                Name = mountaineer.Name,
+                Age = mountaineer.Age,
+                Gender = mountaineer.Gender,
+                LastSeenDate = mountaineer.LastSeenDate
+            };
+            using (var db = new RescueRegisterDbContext())
+            {
+                db.Mountaineers.Add(human);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            using (var db = new RescueRegisterDbContext())
+            {
+                var humanToDelete = db.Mountaineers.FirstOrDefault(t => t.Id == id);
+                if (humanToDelete == null)
+                {
+                    RedirectToAction("Index");
+                }
+                db.Mountaineers.Remove(humanToDelete);
+                db.SaveChanges();
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Mountaineer mountaineer)
+        {
+            // TODO: Implement me
+            return null;
+        }
+    }
+}
---
+++ RescueRegister/Migrations/20190414091220_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace RescueRegister.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Mountaineers",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    age = table.Column<int>(nullable: false),
+                    gender = table.Column<string>(nullable: false),
+                    lastSeenDate = table.Column<string>(nullable: false),
+                    name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mountaineers", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mountaineers");
+        }
+    }
+}
